9d2e5d2999faa537e109d5dfd47902386a58c081
panchongyang <yourhand1025@163.com>
2023-03-19 11:48:12 +0800

    mkdir root

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/ts/index.ts',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'root'),
     },
     module: {
         rules: [
